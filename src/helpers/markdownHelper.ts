0cce1d44de8b0dbacf1cc290bd85ddd7aca69854
Nuno Conceicao <26148760+nunoconc@users.noreply.github.com>
2024-08-29 18:40:40 +0100

    feat: implemented elements and lines markdown syntax

--- src/helpers/markdownHelper.ts
+++ src/helpers/markdownHelper.ts
@@ -1,10 +1,50 @@
+import { elementMarkdowns, lineMarkdowns, type MarkdownSyntax } from '../types/markdownTypes';
+
+
+function replaceLineSyntax(value: string): string {
+    let result = value;
+
+    const lineElement = lineMarkdowns.find((element) => {
+        const results = element.start.exec(result);
+
+        if(!!results?.length) {
+            return element;
+        }
+    })
+
+    if(lineElement) {
+        result = result.replace(lineElement.start, lineElement.tag.open) + lineElement.tag.close;
+    }
+
+    return result;
+}
+
+function replaceElementSyntax(element: MarkdownSyntax, value: string): string {
+    let result = value;
+
+    result = result.replace(element.start, element.tag.open); 
+    result = result.replace(element.end!, element.tag.close); 
+
+    return result;
+}
+
 export const translateMarkdownToHTML = (markdown: string) => {
 
-    return markdown.split(/\s+/).reduce((acc, curr) => {
-        if(curr){
-            acc += curr;
-        }
+    return markdown.split(/\n/).reduce((acc, curr) => {
+        let row = curr;
+        
+        // Adding line syntax, only one per line
+        row = replaceLineSyntax(row);
 
-        return acc;
+        // Adding element syntax, loop for multiple in the same row
+        elementMarkdowns.forEach((element) => {
+            const n = element.start.exec(row)?.length || 0;
+
+            for(let i = 0; i <= n; ++i) {
+                row = replaceElementSyntax(element, row);
+            }
+        })
+
+        return `${acc}<p/>${row}`;
     }, '')
 }
--- src/types/markdownTypes.ts
+++ src/types/markdownTypes.ts
@@ -1,4 +1,4 @@
-enum SyntaxCategories {
+export enum SyntaxCategories {
     'ELEMENT',
     'LINE',
     'GROUP',
@@ -6,43 +6,112 @@ enum SyntaxCategories {
 }
 
 export interface MarkdownSyntax {
-    [key: string]: {
-        category: SyntaxCategories.ELEMENT
-            | SyntaxCategories.LINE
-            | SyntaxCategories.GROUP
-            | SyntaxCategories.BLOCK,
-        tag: {
-            open: string,
-            close: string,
-        }
-        end: string,
-    };
+    key: string,
+    category: SyntaxCategories.ELEMENT
+    | SyntaxCategories.LINE
+    | SyntaxCategories.GROUP
+    | SyntaxCategories.BLOCK,
+    groupTag?: {
+        open: string,
+        close: string,
+    },
+    tag: {
+        open: string,
+        close: string,
+    },
+    start: RegExp,
+    end?: RegExp,
+};
 
-}
-
-export const markdownSyntaxList : MarkdownSyntax = {
-    '#': {
-        category: SyntaxCategories.ELEMENT,
+export const markdownSyntaxList: MarkdownSyntax[] = [
+    {
+        key: 'header-1',
+        category: SyntaxCategories.LINE,
         tag: {
             open: '<h1>',
             close: '</h1>',
         },
-        end: '\n',
+        start: /^#\s/,
+        end: /\r\n/,
     },
-    '##': {
-        category: SyntaxCategories.ELEMENT,
+    {
+        key: 'header-2',
+        category: SyntaxCategories.LINE,
         tag: {
             open: '<h2>',
             close: '</h2>',
         },
-        end: '\n',
+        start: /^##\s/,
+        end: /\r\n/,
     },
-    '###': {
-        category: SyntaxCategories.ELEMENT,
+    {
+        key: 'header-3',
+        category: SyntaxCategories.LINE,
         tag: {
             open: '<h3>',
             close: '</h3>',
         },
-        end: '\n',
+        start: /^###\s/,
+        end: /\r\s /,
+    },
+    {
+        key: 'bold',
+        category: SyntaxCategories.ELEMENT,
+        tag: {
+            open: '<b>',
+            close: '</b>',
+        },
+        start: /\*{2}/,
+        end: /\*{2}/,
+    },
+    {
+        key: 'italic',
+        category: SyntaxCategories.ELEMENT,
+        tag: {
+            open: '<i>',
+            close: '</i>',
+        },
+        start: /\*{1}/,
+        end: /\*{1}/,
+    },
+    {
+        key: 'quote',
+        category: SyntaxCategories.BLOCK,
+        tag: {
+            open: '<blockquote>',
+            close: '</blockquote>',
+        },
+        start: />\s/,
+    },
+    {
+        key: 'unordered-list',
+        category: SyntaxCategories.BLOCK,
+        groupTag: {
+            open: '<ul>',
+            close: '</ul>',
+        },
+        tag: {
+            open: '<li>',
+            close: '</li>',
+        },
+        start: /-\s/,
+    },
+    {
+        key: 'ordered-list',
+        category: SyntaxCategories.BLOCK,
+        groupTag: {
+            open: '<ol>',
+            close: '</ol>',
+        },
+        tag: {
+            open: '<li>',
+            close: '</li>',
+        },
+        start: /\d\.\s/,
     }
-}
+]
+
+
+export const lineMarkdowns = markdownSyntaxList.filter((value) => value.category === SyntaxCategories.LINE);
+export const elementMarkdowns = markdownSyntaxList.filter((value) => value.category === SyntaxCategories.ELEMENT);
+
