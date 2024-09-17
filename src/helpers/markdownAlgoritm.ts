4158572900fcf8632d322410a06eefe651bdf5e5
Nuno Conceicao <26148760+nunoconc@users.noreply.github.com>
2024-09-17 13:11:47 +0100

    feat: working on a new algoritm

---
+++ src/helpers/markdownAlgoritm.ts
@@ -0,0 +1,10 @@
+import type { MarkdownSyntax } from '../types/markdownTypes';
+export const translateMarkdownToHTML = (markdown: string) => {
+
+    const markdownSyntaxOpenList: MarkdownSyntax[] = [];
+    
+    for(let i = 0; i < markdown.length; ++i) {
+        const actualChar = markdown.charAt(i);
+    }
+
+}
